ec965c1cad599e9f4227313c13f1fe857f816832
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-04-28 21:37:28 +0800

    Update main.R

--- main.R
+++ main.R
@@ -7,9 +7,12 @@ library(jsonlite)# for url json format
 
 source("func.R")
 
-stocknum = na.omit(get(getSymbols("2330.TW", from="2000-01-01" ,to=Sys.Date())))
-#sum(is.na(tw2330))
 
+####################################yahoo stock####################################
+stockno <- '2330'
+for (stockno in c("2104.TW", "2330.TW", "2441.TW")){
+stocknum = na.omit(get(getSymbols(stockno, from="2000-01-01" ,to=Sys.Date())))
+#sum(is.na(tw2330))
 chartSeries(stocknum, from="2000-01-01" ,to=Sys.Date(),theme="black")
 
 ma_20<-runMean(stocknum[,4],n=20)
@@ -23,30 +26,31 @@ return<-exp(cumsum(return))
 plot(return)
 addBBands()
 addBBands(draw="p")
+}
 
 
-
-
+##################################### TWSE ###########################################
 qdate <- '20170721'   
 qtype <- 'ALLBUT0999' 
-ttime <- as.character(as.integer(as.POSIXct(Sys.time()))*100)
 url <- paste0('http://www.twse.com.tw/exchangeReport/MI_INDEX?',
               'response=json&date=',qdate,'&type=',qtype,'&_=',ttime)
-
-x <- fromJSON(url,flatten=T)
-x$data5
-
-
 url_2 <- paste0('http://mops.twse.com.tw/mops/web/t05st22')
 
+historyStock <- NULL
 date_1 <- '20190428'
 stockno <- '2330'
+ttime <- as.character(as.integer(as.POSIXct(Sys.time()))*100)
 
+if( month(Sys.time()) >= 10){
+  today <- paste0( year(Sys.time()), month(Sys.time()), day(Sys.time()) )
+} else{
+  today <- paste0( year(Sys.time()), 0, month(Sys.time()), day(Sys.time()) )
+}
+
+for(date_1 in c('20190201','20190301',today)){
 url_1 <- paste0('http://www.tse.com.tw/exchangeReport/STOCK_DAY?',
                'response=json&date=',date_1,'&stockNo=',stockno,'&_=',ttime)
 jsondata <- fromJSON(url_1)
-
-historyStock <- NULL
 if(jsondata$stat == "OK"){
   tmpStock <- data.frame(jsondata$data[, 1],  
                          jsondata$data[, 4:7],  
@@ -54,7 +58,7 @@ if(jsondata$stat == "OK"){
                          stringsAsFactors = FALSE)
   historyStock <- rbind(historyStock, tmpStock) 
 }
-
+}
 
 colnames(historyStock) <- c("Date", "Open", "High", "Low", "Close", "Volume", "Value")         
 #historyStock$Date <- sapply(historyStock$Date, CNV_DATE)         

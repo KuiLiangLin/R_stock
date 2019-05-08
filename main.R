f1de3a055b5e90e1680a633f8a81faa313935d9f
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-05-08 17:46:55 +0800

    Add files via upload

--- main.R
+++ main.R
@@ -18,3 +18,31 @@ source("DOHLCV.R"); update_DOHLCV(date_set[8:10], stock_list[3:4], file_path)
 
 source("func.R"); rcsv_xts <- read_csv_to_xts(file_name)
 chartSeries(rcsv_xts)
+
+
+library(rvest)
+stats <- read_html("http://mops.twse.com.tw/nas/t21/sii/t21sc03_108_2_0.html", encoding = 'ISO-8859-1')
+
+tmp <- NULL
+df_tmp_k <- NULL
+df_tmp_j <- NULL
+df_tmp_i <- NULL
+for(k in c(1:30)){
+  for(j in c(3,4,5,6,7,8,9)){
+    for(i in c(1,3,4,5,6,7,8,9,10,11)){
+      asd <- html_nodes(stats, xpath = paste0('//body//center//center//table//tr//td//table[',k,']//tr[2]//td//table//tr[',j,']//td[',i,']'))
+      tmp[i] <- html_text(asd)
+    }
+    if(tmp[11] == "-"){
+      tmp[1] <- as.numeric(gsub(',', replacement = '', tmp[1]))
+      for(i in c(3:10)){tmp[i] <- as.numeric(gsub(',', replacement = '', tmp[i]))}
+      
+      df_tmp_i <- rbind.data.frame(c(tmp[1], tmp[3:10]))
+      colnames(df_tmp_i) <- c("1", "2", "3", "4", "5", "6", "7", "8", "9") 
+      df_tmp_j <- rbind(df_tmp_j, df_tmp_i) 
+    } else{
+      break
+    }
+  }
+  df_tmp_k <- rbind(df_tmp_k, df_tmp_j) 
+}

782007245c4e28f130e332f76cf7e355b11b5af4
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-05-09 16:41:06 +0800

    Add files via upload

--- main.R
+++ main.R
@@ -17,18 +17,19 @@ if(dir.exists(file_path) == FALSE){ dir.create(file_path)}
 source("DOHLCV.R"); update_DOHLCV(date_set[8:10], stock_list[3:4], file_path)
 
 source("func.R"); rcsv_xts <- read_csv_to_xts(file_name)
-chartSeries(rcsv_xts)
+#chartSeries(rcsv_xts)
 
 
 library(rvest)
 stats <- read_html("http://mops.twse.com.tw/nas/t21/sii/t21sc03_108_2_0.html", encoding = 'ISO-8859-1')
+
 
 tmp <- NULL
 df_tmp_j <- NULL
 df_tmp_i <- NULL
 
 sh_pe <- NULL
-for(k in c(1:30)){
+for(k in c(1:3)){
   j <- 3
   repeat{  # for(j in c(3,4,5,6,7,8,9)){
     for(i in c(1:11)){    # for(i in c(1,3,4,5,6,7,8,9,10,11)){
@@ -52,8 +53,16 @@ for(k in c(1:30)){
     }
     cat(j,",",sep = ""); j <- j + 1
   }
-  source("func.R"); sh_pe <- show_percet_len(sh_pe, k, c(1:30))
+  source("func.R"); sh_pe <- show_percet_len(sh_pe, k, c(1:3))
 }
+
+
+file_path_mon <- paste0(getwd(), "/data_MON/")
+file_name_mon <- paste0(file_path_mon, "10802",".csv")
+if(dir.exists(file_path_mon) == FALSE){dir.create(file_path_mon)}
+write.table(df_tmp_j, file = file_name_mon, sep=",", col.names = FALSE,row.names = FALSE, append = FALSE)
+rcsv <- read.csv(file_name_mon)
+colnames(rcsv) <- c("1", "2", "3", "4", "5", "6", "7", "8", "9") 
 
 #season report
 #http://mops.twse.com.tw/mops/web/ajax_t163sb04?encodeURIComponent=1&step=1&firstin=1&off=1&TYPEK=sii&year=103&season=01

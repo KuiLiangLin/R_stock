6b13e1c951ca156cb7e4ddea9f26dcf2d068d8b5
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-05-10 18:02:48 +0800

    Add files via upload

--- fork_MON_REV_main.R
+++ fork_MON_REV_main.R
@@ -10,10 +10,26 @@ stats <- read_html("http://mops.twse.com.tw/nas/t21/sii/t21sc03_107_12_0.html", 
 # do fork job
 # do fork job
 # do fork job
-df_tmp_j <- NULL
+
+
+out_1 <- NULL
+out_2 <- NULL
+out_3 <- NULL
+df_tmp <- rbind(out_1, out_2, out_3)
 file_name_mon <- "10712"
 file_path_mon <- paste0(getwd(), "/data_MON/")
 file_name_mon_wr <- paste0(file_path_mon, file_name_mon,".csv")
 if(dir.exists(file_path_mon) == FALSE){dir.create(file_path_mon)}
-write.table(df_tmp_j, file = file_name_mon_wr, sep=",",row.names = FALSE, append = FALSE)
-out_1 <- NULL
+write.table(df_tmp, file = file_name_mon_wr, sep=",",row.names = FALSE, append = FALSE)
+
+
+# 
+# aaa <- html_text(html_nodes(stats, xpath = paste0('//body//center//center//table//tr//td//table[1]//tr[2]//td//table')))
+# aaa <- gsub(',', replacement = '', aaa)
+# substring(aaa, 139, 142)
+# bbb <- strsplit(aaa, " ")
+# for(i in c(1:1000)){
+#   is.numeric(bbb[[1]][988])
+# }
+# 
+

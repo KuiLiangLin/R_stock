b2aa11f5e50f0110418dd7c38cc909857179c4cf
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-05-11 21:15:54 +0800

    11

--- fork_MON_REV_main.R
+++ fork_MON_REV_main.R
@@ -21,29 +21,67 @@ if(dir.exists(file_path_mon) == FALSE){dir.create(file_path_mon)}
 write.table(df_tmp, file = file_name_mon_wr, sep=",",row.names = FALSE, append = FALSE)
 
 
-
-
-
- aaa <- html_text(html_nodes(stats, xpath = paste0('//body//center//center//table//tr//td//table[1]//tr[2]//td//table')))
- aaa <- gsub(',', replacement = '', aaa)
- # d <- substring(aaa, 138, 141)
- bbb <- strsplit(aaa, " ")
-d <- NULL
-for(i in c(1:length(bbb[[1]]))){
-  c <- grep("[0-9]", bbb[[1]][i], value = TRUE)
-  if(length(c)==1){d <- cbind(d, c)}
+f1 <- NULL
+for(m in c(1:28)){
+  # m <- 4
+  aaa <- html_text(html_nodes(stats, xpath = paste0('//body//center//center//table//tr//td//table[',m,']//tr[2]//td//table')))
+  aaa <- gsub(',', replacement = '', aaa)
+  # d <- substring(aaa, 138, 141)
+  bbb <- strsplit(aaa, "    ")
+  d <- NULL
+  for(i in c(1:length(bbb[[1]]))){
+    c <- grep("[0-9]", bbb[[1]][i], value = TRUE)
+    if(length(c)==1){d <- cbind(d, c)}
+  }
+  d[1] <- substring(d[1], 139, 142)
+  for(j in c(0:ceiling((length(d)-25)/8))){
+    # d[9+8*j] <- last(strsplit(d[9+8*j], "-")[[1]])
+    # d[9+8*j] <- substring(d[9+8*j], 1, 4)
+    r1 <- strsplit(d[9+8*j], "")
+    cc <- cbind(NA, NA, NA, NA)
+    for(x in c( ceiling(length(r1[[1]])) :1 )){
+      c <- grep("[0-9]", r1[[1]][x], value = TRUE)
+      if(length(c)==1){
+        cc[4] <- cc[3]
+        cc[3] <- cc[2]
+        cc[2] <- cc[1]
+        cc[1] <- c
+        if(is.na(cc[4])==FALSE){break}
+      }
+    }
+    d[9+8*j] <- paste0(cc[1], cc[2], cc[3], cc[4])
+  }
+  f <- c(d[1], d[2])
+  for(k in c(1:ceiling((length(d)-17)/8))){
+    f <- rbind(f, c(d[1+8*k], d[2+8*k]) )
+  }
+  f1 <- rbind(f1, f)
+  print(m)
 }
-d[1] <- substring(d[1], 139, 142)
-# d[9] <- gsub(',', replacement = '', aaa)
-for(j in c(0:(length(d)-9)/8)){
-  d[9+8*j] <- last(strsplit(d[9+8*j], "-")[[1]])
-  d[9+8*j] <- substring(d[9+8*j], 1, 4)
-}
-f <- c(d[1], d[2])
-for(k in c(1:ceiling((length(d)-9)/8))){
-  f <- rbind(f, c(d[1+8*k], d[2+8*k]) )
-}
 
-c(d[1], d[2])
-d[9]
-d[17]
+
+
+
+# 
+# 
+# 
+# c(d[1], d[2])
+# d[9]
+# d[17]
+# r <- strsplit(aaa, "       ")
+# r[[1]][192]
+# r1 <- strsplit(r[[1]][192], "")
+# 
+# r1 <- strsplit(d[9+8*j], "")
+# cc <- cbind(NA, NA, NA, NA)
+# for(x in c(length(r1[[1]]):1)){
+# c <- grep("[0-9]", r1[[1]][x], value = TRUE)
+# if(length(c)==1){
+#   cc[4] <- cc[3]
+#   cc[3] <- cc[2]
+#   cc[2] <- cc[1]
+#   cc[1] <- c
+#   if(is.na(cc[4])==FALSE){break}
+#   }
+# }
+# d[9+8*j] <- paste0(cc[1], cc[2], cc[3], cc[4])
--- main.R
+++ main.R
@@ -6,24 +6,24 @@ source("func.R")
 source("DOHLCV.R")
 
 
-# source("func.R"); stock_list <- get_stock_num_list("20190429")
-# source("func.R"); date_set <- get_date_set_all(2000)
+ source("func.R"); stock_list <- get_stock_num_list("20190429")
+ source("func.R"); date_set <- get_date_set_all(2000)
 
-# file_path <- paste0(getwd(), "/data_DOHLCV/")
-# file_name <- paste0(file_path, stock_list[4],".csv")
-# if(dir.exists(file_path) == FALSE){ dir.create(file_path)}
-# source("DOHLCV.R"); update_DOHLCV(date_set[8:10], stock_list[3:4], file_path)
-# source("func.R"); rcsv_xts <- read_csv_to_xts(file_name)
-# #chartSeries(rcsv_xts)
+ file_path <- paste0(getwd(), "/data_DOHLCV/")
+ file_name <- paste0(file_path, stock_list[4],".csv")
+ if(dir.exists(file_path) == FALSE){ dir.create(file_path)}
+ source("DOHLCV.R"); update_DOHLCV(date_set[8:10], stock_list[3:4], file_path)
+ source("func.R"); rcsv_xts <- read_csv_to_xts(file_name)
+#chartSeries(rcsv_xts)
 
 
 
 
 # 
- library(compiler)
+ # library(compiler)
  library(rvest)
- stats <- read_html("http://mops.twse.com.tw/nas/t21/sii/t21sc03_108_2_0.html", encoding = 'ISO-8859-1')
- file_name_mon <- "10802"
+ stats <- read_html("http://mops.twse.com.tw/nas/t21/sii/t21sc03_108_3_0.html", encoding = 'ISO-8859-1')
+ file_name_mon <- "10803"
  source("MON_REV.R"); month_report_revenue(stats,file_name_mon)
 # rcsv <- read.csv(file_name_mon)
 # colnames(rcsv) <- c("num", "revenue") 

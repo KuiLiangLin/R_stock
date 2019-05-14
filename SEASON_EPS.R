1ec1358ad933c4669dbd60f90cdd89fc34480794
Kuiliang Lin <kuiliang.03g@g2.nctu.edu.tw>
2019-05-14 17:34:29 +0800

    514

--- SEASON_EPS.R
+++ SEASON_EPS.R
@@ -1,7 +1,7 @@
 library(rvest)
 
 season_report_eps <- function(file_name_season, y, z) {
-  # y <- 99
+    # y <- 99
   # http://mops.twse.com.tw/mops/web/ajax_t51sb08?encodeURIComponent=1&step=1&firstin=1&off=1&TYPEK=sii&year=101&season=01
   #"http://mops.twse.com.tw/mops/web/ajax_t163sb04?encodeURIComponent=1&step=1&firstin=1&off=1&TYPEK=sii&year=106&season=01"
   # url <- paste0("http://mops.twse.com.tw/mops/web/ajax_t163sb04?encodeURIComponent=1&step=1&firstin=1&off=1&TYPEK=sii&year=",y,"&season=0",z)
@@ -14,20 +14,25 @@ season_report_eps <- function(file_name_season, y, z) {
   
 
   if(y=='101' | y=='100' | y=='99'){ # year < 102
-    f <- html_text(html_nodes(stats, xpath = paste0('//body//center[3]//table//tr[1]')))
+    f <- html_text(html_nodes(stats, xpath = paste0('//body//center[3]//table//tr[1]//th')))
   }else{
     f <- html_text(html_nodes(stats, xpath = paste0('//body//table[3]//tr[1]//th')))
   }
   
-  for(x in c(2:2000)){
+  for(x in c(2:1200)){
+     # x <- 500
     if(y=='101' | y=='100' | y=='99'){ # year < 102
-      aaa <- html_text(html_nodes(stats, xpath = paste0('//body//center[3]//table//tr[',x,']')))
+      aaa <- html_text(html_nodes(stats, xpath = paste0('//body//center[3]//table//tr[',x,']//td')))
+      if((length(aaa)==0)==FALSE){
+        for(xx in c(1:length(aaa))){aaa[xx] <- gsub(',', replacement = '', aaa[xx])}
+        f <- rbind(f, aaa )
+      }
     }else{
       aaa <- html_text(html_nodes(stats, xpath = paste0('//body//table[3]//tr[',x,']//td')))
+      if(length(aaa)==0){cat("Get",x,"data ");break}
+      for(xx in c(1:length(aaa))){aaa[xx] <- gsub(',', replacement = '', aaa[xx])}
+      f <- rbind(f, aaa )
     }
-    
-    if(length(aaa)==0){cat("Get",x,"data ");break}
-    f <- rbind(f, aaa )
   }
   file_path_season <- paste0(getwd(), "/data_SEASON/")
   # file_name_season <- paste0(y,"0",z)
